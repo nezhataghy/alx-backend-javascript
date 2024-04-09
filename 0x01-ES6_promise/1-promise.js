b48d18bc2407630bac2d0f6d7cefaa0cf319e22d
nezhataghy <nezhataghy@gmail.com>
2024-04-09 23:47:55 +0000

    Don't make a promise...

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+export default function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (!success) return reject(new Error('The fake API is not working currently'));
+    return resolve({ status: 200, body: 'Success' });
+  });
+}
