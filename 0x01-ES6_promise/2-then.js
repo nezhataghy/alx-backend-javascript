e0c769a034622ba7f962d721673f13306675a437
nezhataghy <nezhataghy@gmail.com>
2024-04-10 00:47:00 +0000

    Catch me if you can!

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 export default function handleResponseFromAPI(promise) {
+  console.log('Got a response from the API');
   return promise
     .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => new Error())
-    .finally(() => console.log('Got a response from the API'));
+    .catch(() => new Error());
 }
