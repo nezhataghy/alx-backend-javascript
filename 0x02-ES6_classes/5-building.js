998abde486c8395adbfc4e07a0e88fb562d6be2e
nezhataghy <nezhataghy@gmail.com>
2024-04-11 16:17:09 +0000

    A Building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw TypeError('Sqft must be a number');
+    }
+    this._sqft = newSqft;
+  }
+}
