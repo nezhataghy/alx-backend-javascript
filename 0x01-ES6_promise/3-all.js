9ffcc69a3e4fcf7e81c21984769d1e26d41efd75
nezhataghy <nezhataghy@gmail.com>
2024-04-11 00:31:34 +0000

    Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,6 @@
-#!/usr/bin/node
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup () {
+export default function handleProfileSignup() {
   return Promise
     .all([uploadPhoto(), createUser()])
     .then((res) => {
