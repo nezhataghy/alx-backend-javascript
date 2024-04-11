ea5bfc915f2e096054fc0e5845780f628d7175d5
nezhataghy <nezhataghy@gmail.com>
2024-04-11 15:48:05 +0000

    A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,1 +1,45 @@
-#!/usr/bin/node
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
+      throw TypeError('Students must be an array');
+    }
+    for (const student of newStudents) {
+      if (typeof student !== 'string') {
+        throw TypeError('Students must be an array of strings');
+      }
+    }
+    this._students = newStudents;
+  }
+}
