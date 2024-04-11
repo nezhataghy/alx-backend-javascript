4bcb1ff48da911ef9edd6ec602aebf183c971703
nezhataghy <nezhataghy@gmail.com>
2024-04-11 01:00:48 +0000

    Throw an error

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const res = mathFunction();
+    queue.push(res);
+  } catch (error) {
+    queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
