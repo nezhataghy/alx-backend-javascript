c817b15e151832fef1b711347a1be03df5a7b75e
nezhataghy <nezhataghy@gmail.com>
2024-04-12 01:40:20 +0000

    Hoisting

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,32 +1,32 @@
 export default class HolbertonClass {
-  constructor (size, location) {
+  constructor(size, location) {
     this.size = size;
     this.location = location;
   }
 
-  get size () {
+  get size() {
     return this._size;
   }
 
-  set size (newSize) {
+  set size(newSize) {
     if (typeof newSize !== 'number') {
       throw TypeError('Size must be a number');
     }
     this._size = newSize;
   }
 
-  get location () {
+  get location() {
     return this._location;
   }
 
-  set location (newLocation) {
+  set location(newLocation) {
     if (typeof newLocation !== 'string') {
       throw TypeError('Location must be a string');
     }
     this._location = newLocation;
   }
 
-  [Symbol.toPrimitive] (hint) {
+  [Symbol.toPrimitive](hint) {
     if (hint === 'number') {
       return this._size;
     } if (hint === 'string') {
