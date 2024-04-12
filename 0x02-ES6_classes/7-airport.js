68fbf7540f14018751b3b4d71a42849496f435d3
nezhataghy <nezhataghy@gmail.com>
2024-04-12 01:28:23 +0000

    Airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,1 +1,32 @@
-#!/usr/bin/node
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw TypeError('Code must be a string');
+    }
+    this._code = newCode;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
