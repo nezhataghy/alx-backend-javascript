84cd1050023905c801bcf7980b6e1c7d4a2c2686
nezhataghy <nezhataghy@gmail.com>
2024-04-12 01:43:31 +0000

    Vroom & EVCar

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    return new this.constructor[Symbol.species]();
+  }
+}
--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  cloneCar() {
+    const Spec = super.constructor[Symbol.species];
+    return new Spec();
+  }
+}
