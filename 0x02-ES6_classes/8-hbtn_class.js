c5fe303f50d92e3a49904b95a5cb427ac4f2eb5f
nezhataghy <nezhataghy@gmail.com>
2024-04-12 01:33:05 +0000

    Primitive - Holberton Class

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,1 +1,37 @@
-#!/usr/bin/node
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+}
+
+  get size() {
+    return this._size;
+}
+
+  set size(newSize) {
+    if (typeof newSize !== 'number') {
+      throw TypeError('Size must be a number');
+    }
+    this._size = newSize;
+}
+
+  get location() {
+    return this._location;
+}
+
+  set location(newLocation) {
+    if (typeof newLocation !== 'string') {
+      throw TypeError('Location must be a string');
+    }
+    this._location = newLocation;
+}
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    } if (hint === 'string') {
+      return this._location;
+    }
+    return this;
+  }
+}
