fdc72668e4284f6df99616fc00d384e5b5fc101b
nezhataghy <nezhataghy@gmail.com>
2024-04-13 00:22:02 +0000

    Iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,16 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let result = '';
-  
-  for (const key in reportWithIterator) {
-    const employees = reportWithIterator[key];
-    if (Array.isArray(employees)) {
-      result += employees.join(' | ');
-      result += ' | ';
-    }
-  }
-  
-  // Remove the trailing ' | ' from the end of the string
-  result = result.slice(0, -3);
-  
-  return result;
+  return reportWithIterator.join(' | ');
 }
