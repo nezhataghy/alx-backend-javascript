b839b27b02a3b3ed6d25bb5aca7842310e3d50a1
nezhataghy <nezhataghy@gmail.com>
2024-04-16 23:40:53 +0000

    Combine

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  return arr
+    .filter((std) => std.location === city)
+    .map((std) => ({
+      ...std,
+      grade:
+          newGrades
+            .filter((g) => g.studentId === std.id)
+            .map((g) => g.grade)[0] || 'N/A',
+    }));
+}
