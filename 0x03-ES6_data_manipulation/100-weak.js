8d808a0c22588222648bfae867bcb3b118f61fa1
nezhataghy <nezhataghy@gmail.com>
2024-04-16 23:42:38 +0000

    Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+export const weakMap = new WeakMap();
+let callTime = 1;
+
+export function queryAPI(endPoint) {
+  weakMap.set(endPoint, callTime);
+  callTime += 1;
+  const callCount = weakMap.get(endPoint);
+  if (callCount >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+}
