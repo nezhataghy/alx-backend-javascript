0ddba81b873b4c25991097a2ca72352267237110
nezhataghy <nezhataghy@gmail.com>
2024-04-16 23:27:22 +0000

    Map data structure

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+export default function groceriesList() {
+  const MyGroceries = new Map();
+  MyGroceries.set('Apples', 10);
+  MyGroceries.set('Tomatoes', 10);
+  MyGroceries.set('Pasta', 1);
+  MyGroceries.set('Rice', 1);
+  MyGroceries.set('Bananas', 5);
+
+  return MyGroceries;
+}
