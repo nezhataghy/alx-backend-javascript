0542dd2fb4745657867091848a4daba53ca485de
nezhataghy <nezhataghy@gmail.com>
2024-04-16 23:12:12 +0000

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
   const Array = [];
   if (startString !== '' && typeof startString === 'string') {
-    for (const element of set) {
-      if (element.startsWith(startString)) {
-        const restOfElement = element.slice(startString.length);
-        Array.push(restOfElement);
+    for (let element of set) {
+      if (element && element.startsWith(startString)) {
+        element = element.replace(startString, '');
+        Array.push(element);
       }
     }
     return Array.join('-');
