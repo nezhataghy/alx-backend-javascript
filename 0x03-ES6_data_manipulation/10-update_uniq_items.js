1bb8b490b506f8cf58fd42972490b47ce7da69a3
nezhataghy <nezhataghy@gmail.com>
2024-04-16 23:42:05 +0000

    More map data structure

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw Error('Cannot process');
+  for (const key of map.keys()) {
+    if (map.get(key) === 1) map.set(key, 100);
+  }
+
+  return map;
+}
